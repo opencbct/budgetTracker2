971b6ce416afcc830ea21f5ca02d7a223e00aff3
Benjamin Kim <84877261+opencbct@users.noreply.github.com>
2021-11-16 21:42:07 -0600

    updated css and index html

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+
+const express = require(`express`);
+const mongoose = require(`mongoose`);
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(`public`));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
-// routes
-app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`));
+// routes
+app.use(require(`./routes/api.js`));
+
+app.listen(PORT, () => console.log(`App running on http://localhost:
